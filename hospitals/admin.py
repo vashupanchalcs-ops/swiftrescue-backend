7607092808294ae8ab4b8005c8ab1a07cd75618c
Vashu Panchal <vashupanchal.cs@gmail.com>
2026-08-07 00:02:08 +0530

    Register hospital staff in Django admin

--- hospitals/admin.py
+++ hospitals/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Hospital
+from .models import Hospital, HospitalStaff
 
 admin.site.register(Hospital)
+
+
+@admin.register(HospitalStaff)
+class HospitalStaffAdmin(admin.ModelAdmin):
+    list_display = ["id", "hospital", "full_name", "role", "is_on_call", "is_active"]
+    search_fields = ["full_name", "email", "contact_number", "specialization", "hospital__name"]
+    list_filter = ["role", "is_on_call", "is_active"]
